7ab59872dfdef1b6122522bda8a873ecca4212e8
Anuja kumari <anujakumari781@gmail.com>
2026-09-11 12:58:55 +0530

    Add files via upload

---
+++ TREES/array_represenation.c
@@ -0,0 +1,12 @@
+//REPRESENTATION OF BINARY TREE
+//                                      7
+//                                     / \
+//                                    /   \
+//                                  11    2
+//                                 /  \  / \
+//                                /    \ 11 9
+//                              7       1    \
+//                             /             11
+//                           2
+
+//[7 , 11,2,7,1,11,9,2,NULL ,NULL, NULL,NULL,NULL,NULL,11]
